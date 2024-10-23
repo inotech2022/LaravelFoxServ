df9659a56e4959ff4b6ad1db236d3dc78a27ae12
inotech2022 <tccinotec@gmail.com>
2024-10-23 12:02:31 -0300

    verificacao de email - sem funcionar

---
+++ resources/views/emails/confirmacao.blade.php
@@ -0,0 +1,7 @@
+<p>Olá, {{ $name }}!</p>
+
+<p>Obrigado por se cadastrar no FoxServ. Para confirmar seu cadastro, clique no link abaixo:</p>
+
+<p><a href="{{ $confirmationLink }}">Confirmar meu cadastro</a></p>
+
+<p>Se você não solicitou este cadastro, ignore este e-mail.</p>
