d3cb2b77eee2baf7f22a3f199ab36d6cd2eb835e
inotech2022 <tccinotec@gmail.com>
2024-11-07 16:28:54 -0300

    Filtros dos profissionais funcinando. A MERDA DO CADASTRO DO PROF N FUNCINA MAIS :)

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,27 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+    
     protected $table = 'services';
     protected $primaryKey = 'serviceId';
-
     public $incrementing = true; 
-
     protected $keyType = 'int';
+
     protected $fillable = [
         'serviceId',
         'serviceName',
         'serviceTypeId'
     ];
 
-    // Relacionamento com ServiceType
     public function serviceType()
     {
         return $this->belongsTo(ServiceType::class, 'serviceTypeId', 'serviceTypeId');
     }
 
-    // Relacionamento muitos-para-muitos com Professional (um serviço pode ser prestado por muitos profissionais)
     public function professionals()
     {
-        return $this->belongsToMany(Professional::class, 'service_professional', 'serviceId', 'professionalId');
+        return $this->belongsToMany(Professional::class, 'service_professionals', 'serviceId', 'professionalId');
     }
 }
