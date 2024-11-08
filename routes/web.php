b060bcc5369bad8f748161e67e2154647b906871
inotech2022 <tccinotec@gmail.com>
2024-11-08 17:50:41 -0300

    Cadastro Prof Resolvido AMEM IRMAOS

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use App\Http\Controllers\ServicosController;
 
 // Define a rota principal
 Route::get('/', [IndexController::class, 'index'])-> name('index');
-Route::post('/index', [IndexController::class, 'store'])-> name('index');
+Route::post('/', [IndexController::class, 'store'])-> name('sugestao');
 Route::post('/denuncia', [PerfilProfissionalController::class, 'store'])->name('denuncia.store');
 Route::get('/avaliacao', [AvaliacaoController::class, 'create'])->name('avaliacao');
 Route::post('/avaliacao', [AvaliacaoController::class, 'store'])->name('avaliacao');
