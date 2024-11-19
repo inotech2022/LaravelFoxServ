4985ae3a7046b702839b95e695070b08e03114a6
inotech2022 <tccinotec@gmail.com>
2024-11-19 19:14:45 -0300

    modais corrigidos

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -88,6 +88,7 @@
                                                 <img class="foto_menu" src="{{ asset($curtida->profilePic) }}" alt="Foto do usuário">
                                                 {{ $curtida->name }} {{ $curtida->lastName }}<br>
                                                 Curtiu sua Publicação
+                                                {{ \Carbon\Carbon::parse($curtida->likeDate)->format('d/m/Y') }}
                                                 <span class="material-symbols-outlined">favorite</span>
                                             </li>
                                             <hr>
@@ -97,7 +98,8 @@
                                             <li>
                                                 <img class="foto_menu" src="{{ asset($avaliacao->profilePic) }}" alt="Foto do usuário">
                                                 {{ $avaliacao->name }} {{ $avaliacao->lastName }}<br>
-                                                Avaliou seu serviço
+                                                Avaliou seu serviço <br>
+                                               {{ \Carbon\Carbon::parse($avaliacao->ratingDate)->format('d/m/Y') }}
                                                 <span class="material-symbols-outlined">star</span>
                                             </li>
                                             <hr>
