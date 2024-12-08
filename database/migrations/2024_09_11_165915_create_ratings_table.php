fb926fa4fa4d9256e9ef0846d3bce76cd75b5d98
inotech2022 <114019233+inotech2022@users.noreply.github.com>
2024-12-08 16:58:34 -0300

    Update 2024_09_11_165915_create_ratings_table.php

--- database/migrations/2024_09_11_165915_create_ratings_table.php
+++ database/migrations/2024_09_11_165915_create_ratings_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->date('ratingDate');
             $table->unsignedInteger('userId'); 
             $table->foreign('userId')->references('userId')->on('users')->onDelete('cascade');
-            $table->unsignedBigInteger('protocol');
+            $table->unsignedInteger('protocol');
             $table->foreign('protocol')->references('protocol')->on('contracts')->onDelete('cascade');
             $table->timestamps();
         });
