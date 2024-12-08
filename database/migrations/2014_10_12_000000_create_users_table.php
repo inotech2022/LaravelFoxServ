2cab1393db61e734c942b6b3550f85d69273c1e6
inotech2022 <114019233+inotech2022@users.noreply.github.com>
2024-12-08 17:03:28 -0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('userId'); 
+            $table->unsignedInteger('userId')->autoIncrement();
             $table->string('name');
             $table->string('lastName');
             $table->char('phone', 11);
