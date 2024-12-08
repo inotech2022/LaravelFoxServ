603b6f08d65a1b035cb52ffc9e41f77d2fa35786
inotech2022 <114019233+inotech2022@users.noreply.github.com>
2024-12-08 17:04:09 -0300

    Update 2024_09_11_102525_create_contracts_table.php

--- database/migrations/2024_09_11_102525_create_contracts_table.php
+++ database/migrations/2024_09_11_102525_create_contracts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('contracts', function (Blueprint $table) {
-            $table->bigIncrements('protocol');
+            $table->unsignedInteger('protocol')->autoIncrement();
             $table->date('registrationDate');
             $table->date('startDate');
             $table->date('endDate');
