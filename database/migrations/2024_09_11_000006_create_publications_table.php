cf0e204a90b3905392df4e0b1fae87d02e15639a
inotech2022 <114019233+inotech2022@users.noreply.github.com>
2024-12-08 18:05:55 -0300

    Update 2024_09_11_000006_create_publications_table.php

--- database/migrations/2024_09_11_000006_create_publications_table.php
+++ database/migrations/2024_09_11_000006_create_publications_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('caption');
             $table->unsignedInteger('professionalId');
             $table->foreign('professionalId')->references('professionalId')->on('professionals')->onDelete('cascade');
-            $table->timestamps();    
+              
         });
     }
 
